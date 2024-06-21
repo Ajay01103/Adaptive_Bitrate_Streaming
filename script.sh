b00c79dc3e98685dbf9c0af7d2f9f40f2aca0706
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-06-21 16:26:11 +0530

    initial commit

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
 docker run -it -v "C:\Users\HARVEER SINGH\Desktop\streaming\videos:/home/app/videos" streaming
+ffmpeg -i KFP.mp4 -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename outputs/segment%03d.ts -start_number 0 outputs/index.m3u8
